52d5565baa6eca5077ffca80fbddab27a1849abf
Jehwan Kim <jehwan.kim@kreimben.com>
2024-02-22 19:45:47 +0900

    Add Transformer and Decoder blocks
    This commit adds the `TransformerBlock` and `DecoderBlock` classes in `model/transformer/block.py`. Both of the classes utilize the previously defined `SelfAttention` module. The `TransformerBlock` includes a feed forward network and applies dropout and normalization, while the `DecoderBlock` includes an additional transformer block.

---
+++ model/transformer/block.py
@@ -0,0 +1,43 @@
+from torch import nn
+
+from model.transformer.module import SelfAttention
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, embed_size, heads, dropout, forward_expansion):
+        super(TransformerBlock, self).__init__()
+        self.attention = SelfAttention(embed_size, heads)
+        self.norm1 = nn.LayerNorm(embed_size)
+        self.norm2 = nn.LayerNorm(embed_size)
+
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embed_size, forward_expansion * embed_size),
+            nn.ReLU(),
+            nn.Linear(forward_expansion * embed_size, embed_size),
+        )
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, value, key, query):
+        attention = self.attention(value, key, query)
+
+        # Add skip connection, followed by normalization
+        x = self.dropout(self.norm1(attention + query))
+        forward = self.feed_forward(x)
+        out = self.dropout(self.norm2(forward + x))
+        return out
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self, embed_size, heads, forward_expansion, dropout, max_length):
+        super(DecoderBlock, self).__init__()
+        self.attention = SelfAttention(embed_size, heads)
+        self.norm = nn.LayerNorm(embed_size)
+        self.transformer_block = TransformerBlock(embed_size, heads, dropout, forward_expansion)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, value, key, src_mask, trg_mask):
+        attention = self.attention(x, x, x, trg_mask)
+        query = self.dropout(self.norm(attention + x))
+        out = self.transformer_block(value, key, query, src_mask)
+        return out
