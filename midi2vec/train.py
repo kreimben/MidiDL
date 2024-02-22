3e995c30da471496f68d8558fb37944102f677ff
Jehwan Kim <jehwan.kim@kreimben.com>
2024-02-22 19:42:50 +0900

    Update training settings and add load function in train.py
    In the midi2vec module's train.py, the configuration and parameters used for model training have been adjusted to optimize performance. Additionally, a new function has been added to allow for the loading of trained models. This will facilitate the reusability and testing of saved models.

--- midi2vec/train.py
+++ midi2vec/train.py
@@ -1,12 +1,25 @@
+import os
+
 import networkx as nx
+from gensim.models import Word2Vec
 from node2vec import Node2Vec
 
+embedding_name = 'midi_embeddings.emb'
+saved_model_name = 'midi_model.model'
+max_cpu_cores = os.cpu_count()
 
-def train_node(G: nx.Graph, demensions=64, walk_length=30, num_walks=200, workers=4, window=10, min_count=1,
-               batch_words=4, save_result=True, embedding_name='midi_embeddings.emb',
-               saved_model_name='midi_model.model'):
-    node2vec = Node2Vec(G, demensions, walk_length, num_walks, workers)
-    model = node2vec.fit(window=window, min_count=min_count, batch_words=batch_words)
-    if save_result:
-        model.wv.save_word2vec_format(embedding_name)
-        model.save(saved_model_name)
+
+def train_node(G: nx.Graph, demensions=64, walk_length=64, num_walks=256, workers=max_cpu_cores - 1, window=16,
+               min_count=1, batch_words=64):
+    node2vec = Node2Vec(G, demensions, walk_length, num_walks, workers, seed=42)
+    model = node2vec.fit(window, min_count, batch_words)
+
+    model.wv.save_word2vec_format(embedding_name)
+    model.save(saved_model_name)
+
+
+def load_trained_node():
+    embedding = Word2Vec.wv.load_word2vec_format(saved_model_name)
+    model = Word2Vec.load(saved_model_name)
+
+    return embedding, model
